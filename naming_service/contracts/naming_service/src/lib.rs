f7894ce168047545f63bcc473d48a1f4d94ae302
mystical-prog <jdgt.vd.0405@gmail.com>
2023-08-01 17:50:18 +0000

    bugs fixed

--- naming_service/contracts/naming_service/src/lib.rs
+++ naming_service/contracts/naming_service/src/lib.rs
@@ -1,133 +1,143 @@
-module NamingService
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
 
-struct DomainInfo {
-    owner: Address
-}
+#[ink::contract]
+pub mod naming_service {
 
-struct Auction {
-    seller: Address
-    highestBidder: Address
-    highestBid: Amount
-    startingBid: Amount
-    tickPrice: Amount
-    buyoutPrice: Amount
-    endTimeStamp: Timestamp
-}
+    use ink::storage::Mapping;
+    use ink::prelude::vec::Vec;
 
-event DomainRegistered {
-    name: String
-    owner: Address
-}
-    
-event DomainTransferred {
-    name: String
-    from: Address
-    to: Address
-}
-    
-event AuctionCreated {
-    name: String
-    endTimeStamp: Timestamp
-}
-    
-event NewBid {
-    name: String
-    bidder: Address
-    bid: Amount
-}
-    
-event AuctionEnded {
-    name: String
-    winner: Address
-    winningBid: Amount
-}
-
-contract NamingService {
-    mapping(String, DomainInfo) domains
-    mapping(String, Auction) auctions
-    daoTreasury: Address
-
-    function isValidDomainName(name: String) -> Boolean {
-        let max_length: usize = 25;
-        let allowed_chars: &[u8] = b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-";
-
-        if name.len() > max_length || name.len() < 1 {
-            return False;
-        }
-
-        for &byte in name {
-            if byte.isWhitespace() {
-                return False;
-            }
-
-            if !allowed_chars.contains(&byte) {
-                return False;
-            }
-        }
-        return True;
+    #[ink(event)]
+    pub struct DomainRegistered {
+        #[ink(topic)]
+        name: Vec<u8>,
+        #[ink(topic)]
+        owner: AccountId,
     }
 
-    function registerDomain(name: String, suffixIdx: u32) -> Boolean {
-        if !isValidDomainName(name) {
-            return False;
-        }
-
-        let name = name + ".star"
-        let owner = caller()
-
-        if domains[name] != Null {
-            returnn False;
-        }
-
-        domains[Name] = DomainInfo(owner)
-        emit DomainRegistered(name, owner)
-        return True
+    #[ink(event)]
+    pub struct DomainTransferred {
+        #[ink(topic)]
+        name: Vec<u8>,
+        #[ink(topic)]
+        from: AccountId,
+        #[ink(topic)]
+        to: AccountId,
     }
 
-    function transferDomain(name: String, newOwner: Address) -> Boolean {
-        if auctions[name] != Null || domains[name] != Null || domains[name].owner != caller() {
-            return False
-        }
-
-        domains[name].owner = newOwner
-        emit DomainTransferred(name, caller(), newOwner)
-        return True
-    }
-
-    function getDomainInfo(String: name) -> DomainInfo? {
-        return domains[name]
-    }
-
-    function createAuction(
-        name: String,
-        duration: Timestamp, 
-        startingBid: Amount, 
-        tickPrice: Amount, 
-        buyoutPrice: Amount
-    ) -> Boolean {
-        if auctions[name] != Null || domains[name] != Null || domains[name].owner != caller() {
-            return True
-        }
-
-        let currentTimeStamp = blockTimeStamp()
-        let endTimeStamp = currentTimeStamp + duration
-
-        auctions[name] = Auction(
-            seller = caller(),
-            highestBidder = caller(),
-            highestBid = startingBid,
-            startingBid = startingBid,
-            tickPrice = tickPrice,
-            buyoutPrice = buyoutPrice,
-            endTimeStamp = endTimeStamp
+    #[derive(scale::Decode, scale::Encode)]
+    #[cfg_attr(
+        feature = "std",
+        derive(
+            Debug,
+            PartialEq,
+            Eq,
+            scale_info::TypeInfo,
+            ink::storage::traits::StorageLayout
         )
-
-        emit AuctionCreated(name, endTimeStamp)
-        return True
+    )]
+    pub struct DomainInfo {
+        owner: AccountId,
     }
 
-    
+    #[ink(storage)]
+    pub struct NamingService {
+        domains: Mapping<Vec<u8>, DomainInfo>,
+        dao_treasury: AccountId,
+    }
 
+    impl NamingService {
+        #[ink(constructor)]
+        pub fn new(dao_treasury: AccountId) -> Self {
+            let domains = Mapping::default();
+            Self {
+                domains,
+                dao_treasury,
+            }
+        }
 
+        #[ink(message, payable)]
+        pub fn register_domain(&mut self, name: Vec<u8>) -> bool {
+            
+            let base = 200_000_000_000_000_000_000;
+            
+            let value = self.env().transferred_value(); 
 
+            if !Self::is_valid_domain_name(&name) || name.len() < 3 {
+                return false;
+            }
+
+            let suffix = ".star".as_bytes().to_vec();
+            let full_name = [&name[..], &suffix[..]].concat();
+            let caller = self.env().caller();
+            
+
+            if self.domains.contains(&full_name) || value < base
+            {
+                return false;
+            }
+
+            self.domains.insert(
+                full_name.clone(),
+                &DomainInfo {
+                    owner : caller,
+                },
+            );  
+
+            self.env().emit_event(DomainRegistered { name: full_name, owner: caller });            
+
+	        if let Err(_) = self.env().transfer(self.dao_treasury, value) {
+                return false;
+            }
+
+            true            
+        }
+
+        #[ink(message)]
+        pub fn transfer_domain(&mut self, name: Vec<u8>, new_owner: AccountId) -> bool {
+        
+            if let Some(mut domain_info) = self.domains.take(&name) {
+                let caller = self.env().caller();
+                if domain_info.owner == caller {
+                    domain_info.owner = new_owner;
+                    self.domains.insert(name.clone(), &domain_info);
+                    self.env().emit_event(DomainTransferred {
+                        name: name.clone(),
+                        from: caller,
+                        to: new_owner,
+                    });
+                    return true;
+                }
+            }
+            false
+        }     
+        
+        #[ink(message)]
+        pub fn get_domain_info(&self, name: Vec<u8>) -> Option<DomainInfo> {
+            self.domains.get(name)
+        }
+        
+        pub fn is_valid_domain_name(name: &[u8]) -> bool {
+            let max_length: usize = 25;
+            let allowed_chars: &[u8] = b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-";
+        
+            // Check if the domain name exceeds the maximum allowed length
+            if name.len() > max_length {
+                return false;
+            }
+        
+            for &byte in name {
+                // Check for whitespace characters
+                if byte.is_ascii_whitespace() {
+                    return false;
+                }
+        
+                // Check for special characters
+                if !allowed_chars.contains(&byte) {
+                    return false;
+                }
+            }
+        
+            true
+        }        
+    }
 }

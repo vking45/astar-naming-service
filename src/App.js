2771734e4854f80716bdacf22ff65d0c967e9c24
namanthanki <stuniz7855@gmail.com>
2023-08-23 17:55:08 +0530

    starting from scratch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import Landing from './views/Landing';
-import Dashboard from './views/Dashboard';
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { Fragment } from 'react';
 
 function App() {
   return (
     <div className="relative min-h-screen">
       <Router>
         <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/home/:name" element={<Dashboard />} />
+          <Route path="/" element={<Fragment />} />
         </Routes>
       </Router>
     </div>
